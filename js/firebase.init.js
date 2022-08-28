d0c8fd795f52c0a894090bbb7e3f59367d798c08
Imran Asif <56230659+imranasif87@users.noreply.github.com>
2022-08-28 15:15:25 +0100

    add firebase

---
+++ js/firebase.init.js
@@ -0,0 +1,9 @@
+﻿// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "hVINZJDrGFeLd3ycBkONeNzIVJZ7WpjjMdcHtTyH",
+    authDomain: "expensedb-7c04f.firebaseapp.com",
+    databaseURL: "https://expensedb-7c04f-default-rtdb.firebaseio.com",
+    projectId: "expensedb-7c04f",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
